8db8f4ff35547e4347dd9bbd3c2556474ec6bc34
CapacitorSet <9286933+CapacitorSet@users.noreply.github.com>
2019-09-18 23:59:23 +0200

    Use secure websocket if possible

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -110,7 +110,7 @@ class AnswersRow extends Component {
 class Game extends Component {
 	constructor(props) {
 		super(props);
-		this.socket = new WebSocket(`ws://${document.location.hostname}/ws?match=${MATCH_ID}`);
+		this.socket = new WebSocket(`${(document.location.protocol == "https:") ? "wss" : "ws"}://${document.location.hostname}/ws?match=${MATCH_ID}`);
 		this.state = {
 			// Navbar state
 			timerState: {
